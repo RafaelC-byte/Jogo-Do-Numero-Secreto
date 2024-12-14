d9fbaf33880b50c66a4ef8b14b22b8cab7f01c61
Rafael Costa <rcossilva@icloud.com>
2024-12-14 08:59:18 -0300

    Mudando a dificuldade

--- js-curso-2-aula1/js-curso-2-aula_-/app.js
+++ js-curso-2-aula1/js-curso-2-aula_-/app.js
@@ -1,6 +1,6 @@
 exibirMensagemInicial();
 let listaDeNumeros = [];
-let numeroLimite = 100
+let numeroLimite = 50;
 let numeroSecreto = gerarNumeroAleatorio();
 let tentativas = 1;
 
@@ -19,7 +19,7 @@ function exibirTextoNaTela(tag, texto) {
 
 function exibirMensagemInicial() {
     exibirTextoNaTela('h1', 'Jogo do Número secreto');
-    exibirTextoNaTela('p', 'Escolha um numero entre 1 e 100');
+    exibirTextoNaTela('p', 'Escolha um numero entre 1 e 50');
 }
 function verificarChute() {
     let chute = document.querySelector('input').value;
